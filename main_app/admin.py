f45b27aea4fc434808b5a643c01f0d7f07ebe4f8
Reyesalex777 <Reyesalex777@gmail.com>
2022-11-20 20:54:43 -0800

    update

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Yorkie
+from .models import Yorkie, Feeding
 
 # Register your models here.
 admin.site.register(Yorkie)
+admin.site.register(Feeding)
